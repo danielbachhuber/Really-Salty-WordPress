2136b0d1572a5fe2786ae86f52f8d61a9654ec1c
Daniel Bachhuber <d@danielbachhuber.com>
2014-10-20 10:11:36 -0700

    Don't try to clone if the directory already exists

--- setup.sh
+++ setup.sh
@@ -2,5 +2,9 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-git clone git@github.com:wp-cli/wp-cli.git $DIR/../../wp-cli
+# Set up development version of WP-CLI
+if [ ! -d  "$DIR/../../wp-cli" ]; then
+  git clone git@github.com:wp-cli/wp-cli.git $DIR/../../wp-cli
+fi
+
 ln -sf $DIR/Customfile $DIR/../../Customfile

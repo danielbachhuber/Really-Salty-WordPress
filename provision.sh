fac1e3a652d3f01f5899c9ec0741819a65d437ae
Daniel Bachhuber <d@danielbachhuber.com>
2014-10-22 16:57:42 -0700

    Hook up Pantheon CLI too

--- provision.sh
+++ provision.sh
@@ -2,3 +2,6 @@
 
 # Sets WP-CLI dev to be the default `wp`
 ln -sf /home/vagrant/.wp-cli/bin/wp /usr/local/bin/wp
+
+# Use Pantheon CLI as `p`
+ln -sf /home/vagrant/.pantheon-cli/bin/terminus /usr/local/bin/p
